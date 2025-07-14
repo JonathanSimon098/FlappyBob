c4a69ddb3940e9c8b648a5019d387ed2254fd81b
JonathanSimon098 <jonedu1130@gmail.com>
2025-07-13 23:00:47 -0400

    Added Game Over Screen and game reset functionality

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,19 +4,28 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logicScript;
+    public bool birdIsAlive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.name = "Bob Birdongton";
+        logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
         {
             myRigidbody.linearVelocity = Vector2.up * flapStrength;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        birdIsAlive = false;
+        logicScript.gameOver();
+    }
+
 }
